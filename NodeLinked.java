83b06af7104cc982bd52ba04b677492384fd060d
Anggi <anggiridc21@gmail.com>
2024-12-18 22:10:28 +0800

    Update NodeLinked.java
    menambahkan atribut gaji

--- NodeLinked.java
+++ NodeLinked.java
@@ -1,12 +1,14 @@
 public class NodeLinked{
     String nama_pegawai;
     String posisi;
+    int gaji;
     NodeLinked next;
     NodeLinked prev;
 
     public NodeLinked(String nama_pegawai,String posisi){
         this.nama_pegawai=nama_pegawai;
         this.posisi=posisi;
+        this.gaji = gaji;
         this.next = null;
         this.prev = null;
     }
